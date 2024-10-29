4ab4f057857f3a4c43a281105c9501b1bf6778c5
Manas Dhir <manas.dhir2004@gmail.com>
2024-10-29 23:25:39 +0530

    0.5 threshold to argmax

--- sample_codes_for_participants/Eval_metrics_gen_excel.py
+++ sample_codes_for_participants/Eval_metrics_gen_excel.py
@@ -1,99 +1,80 @@
+import os
 import numpy as np
 import pandas as pd
-from sklearn.metrics import classification_report, roc_auc_score
 import json
-from sklearn.metrics import classification_report, roc_auc_score, precision_recall_curve, auc, recall_score, f1_score, balanced_accuracy_score
+from sklearn.metrics import (
+    roc_auc_score, classification_report, precision_recall_curve, 
+    recall_score, f1_score, auc, balanced_accuracy_score
+)
+
+VALID_CLASSES = [
+    'Angioectasia', 'Bleeding', 'Erosion', 'Erythema', 
+    'Foreign Body', 'Lymphangiectasia', 'Normal', 
+    'Polyp', 'Ulcer', 'Worms'
+]
 
 def save_predictions_to_excel(image_paths, y_pred, output_path):
-    class_columns = ['Angioectasia', 'Bleeding', 'Erosion', 'Erythema', 'Foreign Body', 'Lymphangiectasia', 'Normal', 'Polyp', 'Ulcer', 'Worms']
     y_pred_classes = np.argmax(y_pred, axis=1)
-    predicted_class_names = [class_columns[i] for i in y_pred_classes]
-    df_prob = pd.DataFrame(y_pred, columns=class_columns)
+    predicted_class_names = [VALID_CLASSES[i] for i in y_pred_classes]
+    df_prob = pd.DataFrame(y_pred, columns=VALID_CLASSES)
     df_prob.insert(0, 'image_path', image_paths)
     df_class = pd.DataFrame({'image_path': image_paths, 'predicted_class': predicted_class_names})
     df_merged = pd.merge(df_prob, df_class, on='image_path')
     df_merged.to_excel(output_path, index=False)
 
-
 def calculate_specificity(y_true, y_pred):
     tn = np.sum((y_true == 0) & (y_pred == 0))
     fp = np.sum((y_true == 0) & (y_pred == 1))
-    specificity = tn / (tn + fp) if (tn + fp) > 0 else 0
-    return specificity
+    return tn / (tn + fp) if (tn + fp) > 0 else 0
 
 def generate_metrics_report(y_true, y_pred):
-    class_columns = ['Angioectasia', 'Bleeding', 'Erosion', 'Erythema', 'Foreign Body', 'Lymphangiectasia', 'Normal', 'Polyp', 'Ulcer', 'Worms']
-    metrics_report = {}
-    
     y_true_classes = np.argmax(y_true, axis=1)
     y_pred_classes = np.argmax(y_pred, axis=1)
-    
-    class_report = classification_report(y_true_classes, y_pred_classes, target_names=class_columns, output_dict=True, zero_division=0)
-    
+    class_report = classification_report(
+        y_true_classes, y_pred_classes, target_names=VALID_CLASSES, 
+        output_dict=True, zero_division=0
+    )
     auc_roc_scores = {}
-    for i, class_name in enumerate(class_columns):
+    for i, class_name in enumerate(VALID_CLASSES):
         try:
             auc_roc_scores[class_name] = roc_auc_score(y_true[:, i], y_pred[:, i])
         except ValueError:
-            auc_roc_scores[class_name] = 0.0  
-    
+            auc_roc_scores[class_name] = 0.0
     mean_auc_roc = np.mean(list(auc_roc_scores.values()))
-    auc_roc_scores['mean_auc'] = mean_auc_roc
-    
     specificity_scores = {}
-    for i, class_name in enumerate(class_columns):
-        specificity_scores[class_name] = calculate_specificity(y_true[:, i], (y_pred[:, i] >= 0.5).astype(int))  # Thresholding y_pred
-    
+    for i, class_name in enumerate(VALID_CLASSES):
+        specificity_scores[class_name] = calculate_specificity(y_true[:, i], y_pred_classes == i)
     mean_specificity = np.mean(list(specificity_scores.values()))
-    specificity_scores['mean_specificity'] = mean_specificity
-    
     average_precision_scores = {}
-    for i, class_name in enumerate(class_columns):
+    for i, class_name in enumerate(VALID_CLASSES):
         try:
             precision, recall, _ = precision_recall_curve(y_true[:, i], y_pred[:, i])
             average_precision_scores[class_name] = auc(recall, precision)
         except ValueError:
-            average_precision_scores[class_name] = 0.0  
-    
+            average_precision_scores[class_name] = 0.0
     mean_average_precision = np.mean(list(average_precision_scores.values()))
-    average_precision_scores['mean_average_precision'] = mean_average_precision
-    
-    sensitivity_scores = {}
-    for i, class_name in enumerate(class_columns):
-        try:
-            sensitivity_scores[class_name] = recall_score(y_true[:, i], (y_pred[:, i] >= 0.5).astype(int), zero_division=0)
-        except ValueError:
-            sensitivity_scores[class_name] = 0.0  
-    
+    sensitivity_scores = {class_name: class_report[class_name]['recall'] for class_name in VALID_CLASSES}
     mean_sensitivity = np.mean(list(sensitivity_scores.values()))
-    sensitivity_scores['mean_sensitivity'] = mean_sensitivity
-    
-    f1_scores = {}
-    for i, class_name in enumerate(class_columns):
-        try:
-            f1_scores[class_name] = f1_score(y_true[:, i], (y_pred[:, i] >= 0.5).astype(int), zero_division=0)
-        except ValueError:
-            f1_scores[class_name] = 0.0  
-    
+    f1_scores = {class_name: class_report[class_name]['f1-score'] for class_name in VALID_CLASSES}
     mean_f1_score = np.mean(list(f1_scores.values()))
-    f1_scores['mean_f1_score'] = mean_f1_score
-    balanced_accuracy_scores = balanced_accuracy_score(y_true_classes, y_pred_classes)
+    balanced_acc = balanced_accuracy_score(y_true_classes, y_pred_classes)
+    metrics_report = {
+        'class_report': class_report,
+        'auc_roc_scores': auc_roc_scores,
+        'specificity_scores': specificity_scores,
+        'average_precision_scores': average_precision_scores,
+        'sensitivity_scores': sensitivity_scores,
+        'f1_scores': f1_scores,
+        'mean_auc': mean_auc_roc,
+        'mean_specificity': mean_specificity,
+        'mean_average_precision': mean_average_precision,
+        'mean_sensitivity': mean_sensitivity,
+        'mean_f1_score': mean_f1_score,
+        'balanced_accuracy': balanced_acc
+    }
+    return json.dumps(metrics_report, indent=4)
 
-    metrics_report.update(class_report)
-    metrics_report['auc_roc_scores'] = auc_roc_scores
-    metrics_report['specificity_scores'] = specificity_scores
-    metrics_report['average_precision_scores'] = average_precision_scores
-    metrics_report['sensitivity_scores'] = sensitivity_scores
-    metrics_report['f1_scores'] = f1_scores
-    metrics_report['mean_auc'] = mean_auc_roc
-    metrics_report['mean_specificity'] = mean_specificity
-    metrics_report['mean_average_precision'] = mean_average_precision
-    metrics_report['mean_sensitivity'] = mean_sensitivity
-    metrics_report['mean_f1_score'] = mean_f1_score
-    metrics_report['balanced_accuracy'] = balanced_accuracy_scores
-    
-    metrics_report_json = json.dumps(metrics_report, indent=4)
-    return metrics_report_json
-
-
-
+def process_predictions(image_paths, y_true, y_pred, output_excel_path):
+    save_predictions_to_excel(image_paths, y_pred, output_excel_path)
+    report = generate_metrics_report(y_true, y_pred)
+    print(report)
